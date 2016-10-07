aef9b82fcdffc90decc45d349aca25632146e193
Dayalan <Dayalan>
2016-10-07 11:20:03 +0530

    First Commit

---
+++ src/main/java/com/example/jcoco/Student.java
@@ -0,0 +1,25 @@
+package com.example.jcoco;
+
+public class Student {
+	private int id;
+	private String name;
+	private int score;
+	
+	public Student(int id, String name, int score) {
+		this.id = id;
+		this.name = name;
+		this.score = score;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+}
